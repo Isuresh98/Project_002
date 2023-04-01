f3b0f1c579ab499a0071b0ef1aec5d794fe5b05f
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-04-01 20:29:57 +0530

    BG and BG parralax effect set the Enviroment

---
+++ Assets/Script/Camera/ParallaxBackground.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    public Camera mainCamera;
+    public float parallaxSpeed;
+
+    private float startPosX;
+    private float length;
+
+    void Start()
+    {
+        startPosX = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float temp = (mainCamera.transform.position.x * (1 - parallaxSpeed));
+        float dist = (mainCamera.transform.position.x * parallaxSpeed);
+
+        transform.position = new Vector3(startPosX + dist, transform.position.y, transform.position.z);
+
+        if (temp > startPosX + length)
+        {
+            startPosX += length;
+        }
+        else if (temp < startPosX - length)
+        {
+            startPosX -= length;
+        }
+    }
+}
