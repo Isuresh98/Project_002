f2d7476e9e8ccec727e2c9a1f8d481d574b85f4d
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-04-04 02:52:27 +0530

    UI manager And Level Load Logic add

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
