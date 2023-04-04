d48b7765f354074c9ca9d5bf36e9dee5d7daf339
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-04-04 13:33:13 +0530

    album URL and Start Button Update and animation setup, UI pannel  Asser add

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string url;
     void Start()
     {
     }
@@ -19,5 +20,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(1);
 
+    }
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
     }
 }
