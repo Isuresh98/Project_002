df4c3ee4643473a7fbf3f38b08a8099b0b68811d
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-04-08 08:41:40 +0530

    bike level game over logic complete

---
+++ Assets/Script/Player/Hed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hed : MonoBehaviour
+{
+    private PlayerController_Bike playerBikeScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBikeScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController_Bike>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+
+            print("Hed hit the Ground");
+            playerBikeScript.maxHealth = 0;
+        }
+    }
+}
