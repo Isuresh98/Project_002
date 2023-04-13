fa77ee7c9a5ec7eeccce422aba374d2d1be9ccf8
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-04-13 13:42:30 +0530

    ad the main menu Coin Collection

--- Assets/Script/MainMenu.cs
+++ Assets/Script/MainMenu.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public int CoinCollectamountDisplay;
+    public Text coinText;
+
     public GameObject MainPannel;
     public GameObject LevelPannel;
 
     public string url;
     void Start()
     {
+        CoinCollectamountDisplay = PlayerPrefs.GetInt("Coins", 0);
+        coinText.text = CoinCollectamountDisplay.ToString();
         LevelPannel.SetActive(false);
         MainPannel.SetActive(true);
     }
